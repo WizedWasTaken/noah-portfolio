9907cfb3e314eef34bcf789f073c136dd483871b
WizedWasTaken <jegerfattig1@gmail.com>
2023-11-06 14:59:29 +0100

    Added typing animation

--- src/components/FirstPage.vue
+++ src/components/FirstPage.vue
@@ -17,13 +17,70 @@
         <a href="#github">Github</a>
       </nav>
       <div class="right-child">
-        <h2>My <strong>Work</strong></h2>
+        <h2>Mit <strong>Arbejde</strong></h2>
+        <div>
+          <span class="typed-text">{{ typeValue }}</span>
+      <span class="blinking-cursor">|</span>
+      <span class="cursor" :class="{ typing: typeStatus }">&nbsp;</span>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script>
+export default {
+  name: "typeWiriter",
+  data: () => {
+    return {
+      typeValue: "",
+      typeStatus: false,
+      displayTextArray: ["FullStack Udvikler", "Freelancer", "Database Ekspert", "SEO Ekspert", "WordPress Udvikler"],
+      typingSpeed: 100,
+      erasingSpeed: 100,
+      newTextDelay: 2000,
+      displayTextArrayIndex: 0,
+      charIndex: 0,
+    };
+  },
+  props: {},
+  created() {
+    setTimeout(this.typeText, this.newTextDelay + 200);
+  },
+  methods: {
+    typeText() {
+      if (this.charIndex < this.displayTextArray[this.displayTextArrayIndex].length) {
+        if (!this.typeStatus) this.typeStatus = true;
+        this.typeValue += this.displayTextArray[this.displayTextArrayIndex].charAt(
+          this.charIndex
+        );
+        this.charIndex += 1;
+        setTimeout(this.typeText, this.typingSpeed);
+      } else {
+        this.typeStatus = false;
+        setTimeout(this.eraseText, this.newTextDelay);
+      }
+    },
+    eraseText() {
+      if (this.charIndex > 0) {
+        if (!this.typeStatus) this.typeStatus = true;
+        this.typeValue = this.displayTextArray[this.displayTextArrayIndex].substring(
+          0,
+          this.charIndex - 1
+        );
+        this.charIndex -= 1;
+        setTimeout(this.eraseText, this.erasingSpeed);
+      } else {
+        this.typeStatus = false;
+        this.displayTextArrayIndex += 1;
+        if (this.displayTextArrayIndex >= this.displayTextArray.length)
+          this.displayTextArrayIndex = 0;
+        setTimeout(this.typeText, this.typingSpeed + 1000);
+      }
+    },
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 #first-container {
@@ -72,6 +129,7 @@
   .right-child {
     height: calc(100vh - 10vh - 8px);
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
 
@@ -113,5 +171,64 @@
         }
     }
   }
+}
+
+.blinking-cursor {
+  font-size: 3rem;
+  color: #2c3e50;
+  -webkit-animation: 1s blink step-end infinite;
+  -moz-animation: 1s blink step-end infinite;
+  -ms-animation: 1s blink step-end infinite;
+  -o-animation: 1s blink step-end infinite;
+  animation: 1s blink step-end infinite;
+}
+@keyframes blink {
+  from,
+  to {
+    color: transparent;
+  }
+  50% {
+    color: #2c3e50;
+  }
+}
+@-moz-keyframes blink {
+  from,
+  to {
+    color: transparent;
+  }
+  50% {
+    color: #2c3e50;
+  }
+}
+@-webkit-keyframes blink {
+  from,
+  to {
+    color: transparent;
+  }
+  50% {
+    color: #2c3e50;
+  }
+}
+@-ms-keyframes blink {
+  from,
+  to {
+    color: transparent;
+  }
+  50% {
+    color: #2c3e50;
+  }
+}
+@-o-keyframes blink {
+  from,
+  to {
+    color: transparent;
+  }
+  50% {
+    color: #2c3e50;
+  }
+}
+
+span {
+  font-size: 2rem;
 }
 </style>
