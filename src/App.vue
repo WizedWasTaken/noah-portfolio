e5c3d8ee0939b0daf093f7bc9a62a2d04d6d1c7e
WizedWasTaken <jegerfattig1@gmail.com>
2023-11-06 13:14:18 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import FirstPage from "./components/FirstPage.vue";
+</script>
+
+<template>
+  <FirstPage msg="Vite + Vue" />
+</template>
+
+<style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/components/FirstPage.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="first-container">
+    <div id="left-con">
+      <h1>Portfolio</h1>
+      <div class="img-con">
+        <img
+          src="@/assets/athlete-bodybuilder-with-beautiful-torso-isolated-white-background-naked-torso-african-american-man-posing-studio_397170-2039.png"
+          alt=""
+        />
+      </div>
+    </div>
+    <div id="right-con">
+      <h1>MY WORK</h1>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+#first-container {
+  height: 100vh;
+  display: flex;
+
+  #left-con {
+    width: 30vw;
+    padding: 1rem;
+    background-color: $background-color;
+    text-align: center;
+    color: $secondary-color;
+  }
+
+  #right-con {
+    width: 100%;
+    border-bottom: 5px solid $background-color;
+  }
+
+  .img-con {
+    position: sticky;
+    z-index: 9999;
+    width: 20vw;
+    margin-right: -20vw;
+    height: 80vh;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    img {
+      width: 60%;
+      aspect-ratio: 1 / 1;
+      border-radius: 50%;
+      box-shadow: 0 0 0 10px $background-color, 0 0 0 35px $secondary-color,
+        0 0 0 45px $background-color;
+    }
+  }
+}
+</style>
